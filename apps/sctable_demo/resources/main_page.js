485f71e14d0ad99abb08b0710dc9e7e38f61a4d6
Jonathan Lewis <jslewis1@gmail.com>
2011-03-05 23:35:24 -0500

    Fixing up the demo layout a little.

--- apps/sctable_demo/resources/main_page.js
+++ apps/sctable_demo/resources/main_page.js
@@ -3,10 +3,26 @@
 SCTableDemo.mainPage = SC.Page.design({
 
   mainPane: SC.MainPane.design({
-    childViews: 'tableView'.w(),
+    classNames: 'main-demo-pane',
+    childViews: 'tableView titleView sourceLinkView'.w(),
     
+    sourceLinkView: SC.View.design({
+      classNames: 'source-link-view',
+      layout: { left: 20, top: 30, width: 130, height: 24 },
+      render: function(context, firstTime) {
+        context = context.push('<a href=\"http://www.github.com/jslewis/sctable\">Get the Source!</a>');
+      }
+    }),
+    
+    titleView: SC.LabelView.design({
+      classNames: 'main-page-title',
+      layout: { left: 0, right: 0, top: 0, height: 88 },
+      textAlign: SC.ALIGN_CENTER,
+      value: 'SCTable.TableView'
+    }),
+
     tableView: SCTable.TableView.design({
-      layout: { left: 0, top: 0, bottom: 0, right: 0 },
+      layout: { left: 0, top: 88, right: 0, bottom: 0 },
 
       target: SCTableDemo.tableController,
       action: 'sayHi',
